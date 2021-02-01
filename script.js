7bbfdee3d5115a931f1852638113321f1b345d49
Asif-newaz <asif.newaz0093@gmail.com>
2021-02-02 00:42:10 +0600

    started working with es6

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 // Function constructor
-
 var john = {
     name: `John`,
     yearOfBirth: 1990,
     job: `teacher`
 };
+
+// JavaScript ES6
+// function sayName() {
+//     var name = "Asif Newaz";
+// }
+
+// sayName();
+// console.log(name);
+
+if (true) {
+    var name = "Asif Newaz";
+    // console.log(name)
+}
+
+console.log(name);
