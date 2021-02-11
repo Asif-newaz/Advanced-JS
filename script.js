500233afe6e83ecdbb6b71561a3d1738de8a041c
Asif-newaz <asif.newaz0093@gmail.com>
2021-02-11 23:06:09 +0600

    worked with destructuring

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Function constructor
-var john = {
-  name: `John`,
-  yearOfBirth: 1990,
-  job: `teacher`,
-};
+// var john = {
+//   name: `John`,
+//   yearOfBirth: 1990,
+//   job: `teacher`,
+// };
 
 /* ##########-----JAVASCRIPT ES6 FEATURES BY RYAN CHRISTIANI-----########## */
 
@@ -133,9 +133,9 @@ var john = {
 // console.log(`${firstName} ` .repeat(5));
 
 // Map in JS
-const years = [1980, 1983, 1993, 1997, 1999];
+// const years = [1980, 1983, 1993, 1997, 1999];
 
-console.log(years);
+// console.log(years);
 
 // ES5
 // var ages = years.map(function(year) {
@@ -143,8 +143,69 @@ console.log(years);
 // })
 
 // ES6
-let ages = years.map(
-  (year, xxx) => `Age element ${xxx + 1}: ${2021 - year}`
-);
+// let ages = years.map(
+//   (year, xxx) => `Age element ${xxx + 1}: ${2021 - year}`
+// );
 
-console.log(ages);
+// console.log(ages);
+
+// ES5
+// var box = {
+//   color: "green",
+//   position: 1,
+//   clickMe: function () {
+//  let myself = this;
+//     document.querySelector(".child-1").addEventListener("click", function() {
+//       var str =
+//         "This is the position number " +
+//         myself.position +
+//         " " +
+//         "and it is " +
+//         myself .color;
+//       alert(str);
+//     });
+//   },
+// };
+
+// console.log(box.clickMe());
+
+// const box = {
+//   color: "green",
+//   position: 1,
+//   clickMe: function () {
+//     document.querySelector(".child-1").addEventListener("click", () => {
+//       let str = `This is the position number ${this.position} and it is ${this.color}`;
+//       alert(str);
+//     });
+//   },
+// };
+
+// console.log(box.clickMe());
+
+// Array destructuring
+// ES5
+// var john = ['John', 27];
+// var firstName = john[0];
+// var age = john[1];
+
+// console.log(firstName, age);
+
+// const[firstName, age] = ['John', 27];
+// console.log(firstName);
+// console.log(age);
+
+// Object destructuring
+var obj = {
+  firstName: 'Asif',
+  age: 26
+}
+
+// console.log(obj.firstName);
+
+const {firstName, age} = obj;
+
+// console.log(firstName, age);
+
+const {firstName: a, age: b} = obj;
+
+console.log(b);
