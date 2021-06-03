a406d05f02713b1e119b66cb9b505404cc53f9fb
Asif <asif.newaz0093@gmail.com>
2021-06-03 23:15:29 +0600

    update script file

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var john = {
     name: `John`,
     yearOfBirth: 1990,
-    job: `teacher`
+    job: `teacher`,
 };
 
 // JavaScript ES6
@@ -13,9 +13,30 @@ var john = {
 // sayName();
 // console.log(name);
 
-if (true) {
-    var name = "Asif Newaz";
-    // console.log(name)
-}
+// if (true) {
+//     var name = "Asif Newaz";
+// }
 
+// console.log(name);
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// console.log("Outside loop", i);
+
+// let address = `Sonmania
+//  Kapasia`;
+
+// console.log(address);
+
+let person = {
+    firstName: "Asif",
+    lastName: "Newaz",
+    sayName() {
+        return `My name is ${this.firstName} ${this.lastName}`;
+    },
+};
+
+let name = person.sayName();
 console.log(name);
