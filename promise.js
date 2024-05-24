a9759abf2fffdb1c669451bdea5865c18029fb11
Asif-newaz <asif.newaz0093@gmail.com>
2024-05-25 01:32:06 +0600

    learn: work with promise

--- promise.js
+++ promise.js
@@ -1,25 +1,82 @@
-const status = false;
+// const status = false;
 
-console.log("Task 1");
+// console.log("Task 1");
 
-//promise definition
-const promise = new Promise(function (resolve, reject) {
-  setTimeout(function () {
-    if (status) {
-      resolve("Task 2");
-    } else {
-      reject("Failed!");
-    }
-  }, 2000);
-});
+// //promise definition
+// const promise = new Promise(function (resolve, reject) {
+//   setTimeout(function () {
+//     if (status) {
+//       resolve("Task 2");
+//     } else {
+//       reject("Failed!");
+//     }
+//   }, 2000);
+// });
 
-//promise call
-promise
+// //promise call
+// promise
+//   .then(function (value) {
+//     console.log(value);
+//   })
+//   .catch(function (err) {
+//     console.log(err);
+//   });
+
+// console.log("Task 3");
+
+const paymentSuccess = true;
+const marks = 90;
+
+function enroll() {
+  console.log("Course enrollment is in progress.");
+
+  const promise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      if (paymentSuccess) {
+        resolve();
+      } else {
+        reject("Payment failed!");
+      }
+    }, 2000);
+  });
+
+  return promise;
+}
+
+function progress() {
+  console.log("Course on progress...");
+
+  const promise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      if (marks >= 80) {
+        resolve();
+      } else {
+        reject("You could not get enough marks to get the certificate");
+      }
+    }, 3000);
+  });
+
+  return promise;
+}
+
+function getCertificate() {
+  console.log("Preparing your certificate.");
+
+  const promise = new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve("Congrats! You got the certificate.");
+    }, 1000);
+  });
+
+  return promise;
+}
+
+enroll()
+  .then(progress)
+  .then(getCertificate)
   .then(function (value) {
     console.log(value);
   })
-  .catch(function (value) {
-    console.log(value);
+  .catch(function (err) {
+    console.log(err);
   });
-
-console.log("Task 3");
